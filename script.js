c7d4c203f5c58ec019c84c84bad54b845e9e2eac
balapavithra <pavikannan2991@gmail.com>
2023-07-20 15:00:07 +0530

    Question 1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const numbers = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49];
 
+
 //------------Question 1------------>
+const alter = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49];
+alter.sort(function(a, b){return a - b});
+document.getElementById("qus1").innerHTML = "1) a) Second smallest element in the given array: " + alter[1] +"<br>" + "b) Second largest element in the given array: " + alter[9] ;
+
 
 
 //------------Question 5------------>
